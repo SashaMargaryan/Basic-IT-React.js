4750acf63aee76afbf2da13e2bb86c67ee31835a
SashaMargaryan <sasha.margaryan@inbox.ru>
2020-05-12 17:22:18 +0400

    new Homework-N3

--- Homework-N3/2-xnt.js
+++ Homework-N3/2-xnt.js
@@ -1,12 +1,23 @@
-let arr = [1,2,3,4,5,6,1,2,3];
-let n =0;
-let b;
-for(let i = 0; i < arr.length; i++) {
-    if(arr[i] == arr[i]) {
-        b = arr[i];
-        n++;
-        // console.log(b, n++)
-    }
-    console.log(b, n)
-}
-// console.log(b, n++)
+// let arr = [1,2,3,4,5,6,1,2,3];
+// let n =0;
+// let b;
+// for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] == arr[i]) {
+//         b = arr[i];
+//         n++;
+//         // console.log(b, n++)
+//     }
+//     console.log(b, n)
+// };
+
+var arr = [1, 3, 4, 1, 1, 3, 4, 5];
+var result = {};
+arr.forEach(function(a){
+    if (result[a] != undefined)
+        ++result[a];
+    else
+        result[a] = 1;
+});
+for (var key in result)
+    console.log('namber ' + key + ' == ' + result[key] + ' time');
+
